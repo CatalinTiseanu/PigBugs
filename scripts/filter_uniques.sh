a0988425c07b6165531585a9f71963a4fe013c2b
Ubuntu <ubuntu@ip-10-8-51-30.ec2.internal>
2013-05-09 00:22:09 +0000

    added filter_uniques script and .cpp source

---
+++ scripts/filter_uniques.sh
@@ -0,0 +1,2 @@
+cat part-r* > all_results
+./make_unique < all_results > all_results_filtered
--- scripts/make_unique.cpp
+++ scripts/make_unique.cpp
@@ -3,26 +3,46 @@
 #include <string>
 #include <cstring>
 #include <cstdio>
+#include <iostream>
+
+#include <unordered_map>
 
 using namespace std;
 
 map<string, int> mp;
 
 int main() {
-  char buf[1<<10];
+  char buf[1<<20];
+
+  int nr_processed = 0;
 
   while(gets(buf)) {
+	++nr_processed;
     int count = 0;
     for (int i = 0; i < strlen(buf); ++i) {
-      if (buf[i] == ',') count++;
-      if (count == 2){ count = i; break;}
+      if ((int)buf[i] == 7) count++;
+      if (count == 2) {count = i; break;}
     }
+	if (!count) {
+	  cerr << "error !" << endl;
+	  cerr << buf << endl;
+	  break;
+	}
     string s = string(buf, buf + count);
     if (!mp.count(s)) {
       mp[s] = 1;
       puts(buf);
-    }
+    } else {
+	  //cerr << "Duplicate key " << s << endl; 
+	}
+
+	mp[s]++;
+
+	if (nr_processed % 10000 == 0)
+	  cerr << nr_processed << " lines processed" << endl;
   }
+
+  cerr << "Total size of the map = " << mp.size() << endl;
 
   return 0;
 }
