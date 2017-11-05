f9bf109fad01f97383a04526de1a32a67b643dcb
Catalin Tiseanu <ctiseanu@gmail.com>
2017-11-05 12:52:30 -0800

    Update process.sh

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,6 +1,6 @@
 # author: Catalin-Stefan Tiseanu
-# script for transforming to a .csv file from Pig Forma
-# obviously, we could output it directly to a comma separated format, but doing so introduce inflexibility
+# script for transforming to a .csv file from Pig Format
+# obviously, we could output it directly to a comma separated format, but doing so introduces inflexibility
 # cat .* > all_results
 cat ml_file_ncss | sed -e 's/\t\+/,/g' > all_results_processed_tmp1
 sed 's/(//' all_results_processed_tmp1 > all_results_processed_tmp2
